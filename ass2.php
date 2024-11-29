2a603b9f48a3a16023b0061323457789944dcbc6
omarbadran74 <134789143+omarbadran74@users.noreply.github.com>
2024-11-29 09:50:42 +0300

    Update ass2.php

--- ass2.php
+++ ass2.php
@@ -1,3 +1,5 @@
+omar hany mohamed  20192186 
+
 <?php
 // Define the API URL
 $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
